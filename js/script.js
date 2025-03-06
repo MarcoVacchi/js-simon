78a630e22c89e2bed45a3f57ba0075779a16d066
MarcoVacchi <mvacchi816@gmail.com>
2025-03-06 14:58:48 +0100

    feat: create random numbers

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-
+// funzione contatore;
 let count = 10;
 
 let countDownElement = setInterval (function (){
@@ -12,5 +12,33 @@ let countDownElement = setInterval (function (){
     countElementValue = document.getElementById("countdown").innerHTML = `${count}`;
 
 
-
 }, 1000);
+
+
+// funzione mathrandom che mi restituisce 5 numeri random (numeri da 1 a 50);
+
+    const numbersListElement = document.getElementById ("numbers-list");
+    
+    
+    let myNumber1 = randomNumber ();
+    let myNumber2 = randomNumber ();
+    let myNumber3 = randomNumber ();
+    let myNumber4 = randomNumber ();
+    let myNumber5 = randomNumber ();
+    
+    numbersListElement.innerHTML = 
+    `<li>${myNumber1}<li>
+     <li>${myNumber2}<li>
+     <li>${myNumber3}<li>
+     <li>${myNumber4}<li>
+     <li>${myNumber5}<li>`;
+
+
+
+
+  //  numbersListElement.innerHTML = `<li>${myNumbermath}`;
+
+  function randomNumber (){
+    return Math.floor(Math.random() * 50) + 1
+ }
+  
