066d52603998edbf1eeaa9f4ad98970e0a8281d8
MarcoVacchi <mvacchi816@gmail.com>
2025-03-06 22:08:23 +0100

    feat: complete array

--- js/script.js
+++ js/script.js
@@ -3,9 +3,10 @@ let count = 10;
 const formElement = document.getElementById("answers-form");
 
 
-let countDownElement = setInterval (function (){
+let countDownElement = setInterval(function () {
 
     count--;
+
     if (count === 0) {
         console.log("Sei arrivato a 0");
         numbersListElement.className = "d-none";
@@ -19,28 +20,28 @@ let countDownElement = setInterval (function (){
 
 // funzione mathrandom che mi restituisce 5 numeri random (numeri da 1 a 50);
 
-    const numbersListElement = document.getElementById("numbers-list");
+const numbersListElement = document.getElementById("numbers-list");
 
-    let myNumber1 = randomNumber ();
-    let myNumber2 = randomNumber ();
-    let myNumber3 = randomNumber ();
-    let myNumber4 = randomNumber ();
-    let myNumber5 = randomNumber ();
-    
-    numbersListElement.innerHTML = 
+let myNumber1 = randomNumber();
+let myNumber2 = randomNumber();
+let myNumber3 = randomNumber();
+let myNumber4 = randomNumber();
+let myNumber5 = randomNumber();
+
+numbersListElement.innerHTML =
     `<li>${myNumber1}<li>
      <li>${myNumber2}<li>
      <li>${myNumber3}<li>
      <li>${myNumber4}<li>
      <li>${myNumber5}<li>`;
 
-  //  numbersListElement.innerHTML = `<li>${myNumbermath}`;
+//  numbersListElement.innerHTML = `<li>${myNumbermath}`;
 
-  function randomNumber (){
+function randomNumber() {
     return Math.floor(Math.random() * 50) + 1;
- }
+}
 
- // funzione per inserire i dati quando il contatore arriva a 0 e cambia schermata
+// funzione per inserire i dati quando il contatore arriva a 0 e cambia schermata
 
 // funzione per inserire i dati quando il contatore arriva a 0 e cambia schermata
 const saveNumberElements = document.querySelectorAll(".form-control");
@@ -59,9 +60,8 @@ formElement.addEventListener('submit', function (e) {
 
         let resultNumber = parseInt(saveNumberElements[i].value);
 
-        
         if (checkNumbers.includes(resultNumber)) {
-            newCheckNumbers.push(resultNumber); 
+            newCheckNumbers.push(resultNumber);
         }
     }
 
@@ -71,7 +71,8 @@ formElement.addEventListener('submit', function (e) {
     } else {
         messageElement.innerHTML = "Hai indovinato 0 numeri!";
     }
+
 });
-    
-   
-   
+
+
+
