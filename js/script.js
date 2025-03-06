c120c0e575ef391a2300921ba156623c0db2af7a
MarcoVacchi <mvacchi816@gmail.com>
2025-03-06 16:29:06 +0100

    feat: create cicle

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 // funzione contatore;
 let count = 10;
+const formElement = document.getElementById("answers-form");
+
 
 let countDownElement = setInterval (function (){
 
-
     count--;
-    if (count === 0) {
+    if (count === 9) {
         console.log("Sei arrivato a 0");
         numbersListElement.className = "d-none";
         formElement.className = "d-block";
@@ -13,14 +14,13 @@ let countDownElement = setInterval (function (){
     }
     countElementValue = document.getElementById("countdown").innerHTML = `${count}`;
 
-
 }, 1000);
 
 
 // funzione mathrandom che mi restituisce 5 numeri random (numeri da 1 a 50);
 
     const numbersListElement = document.getElementById("numbers-list");
-    const formElement = document.getElementById("answers-form");
+
     
     
     let myNumber1 = randomNumber ();
@@ -42,10 +42,27 @@ let countDownElement = setInterval (function (){
   //  numbersListElement.innerHTML = `<li>${myNumbermath}`;
 
   function randomNumber (){
-    return Math.floor(Math.random() * 50) + 1
+    return Math.floor(Math.random() * 50) + 1;
  }
 
- // funzione per inserire i dati quando il contatore arriva a 0
+ // funzione per inserire i dati quando il contatore arriva a 0 e cambia schermata
 
+// funzione per inserire i dati quando il contatore arriva a 0 e cambia schermata
+const saveNumberElements = document.querySelectorAll(".form-control");
 
-  
+formElement.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    // Ciclo for per iterare sugli elementi
+    for (let i = 0; i < saveNumberElements.length; i++) {
+
+        let resultNumber = parseInt(saveNumberElements[i].value);
+    console.log(resultNumber);
+    if (resultNumber === myNumber1){
+        console.log('ciao')
+    }  
+    }
+    
+   
+   
+});
